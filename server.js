a61d4adff5ae81841af1ee386fbda6e43a4e197b
skyssa <carcelaarnel@gmail.com>
2023-11-25 01:56:17 +0800

    another commit

--- server.js
+++ server.js
@@ -3,114 +3,119 @@ const http = require("http");
 const socketio = require("socket.io");
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
-const mysql= require("mysql2");
- 
+const mysql = require("mysql2");
+
 const con = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
+
+    host: "localhost",
+    user: "root",
+    password: "",
     database:"chat_app"
+
 });
 
-const io = socketio(server);
 app.use(express.static("client"));
 
 app.use(express.json());
 
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/client/register/index.html");
-});
-
-app.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/client/main/index.html");
-});
-
-app.post("/convo",(req, res)=>{
-    
-    const sql = "SELECT messageid,userid, message_text FROM message";
-    con.query(sql, (err,result)=>{
-        if(!err){
-            return res.status(200).json(result);
-
-        }
-        return res.status(500).json({message:"server Error"});
-    });
-});
-
-
-app.get("/login",(req, res)=>{
     res.sendFile(__dirname + "/client/login.html");
-});
-app.post("/login",(req, res)=>{
-    const {username, userlast , useremail, userpassword}=req.body;
-    const sql = "SELECT userid,username, userlast, useremail FROM register WHERE username=?, userlast=? , useremail=?, userpassword=?";
-    con.query(sql, [username, userlast , useremail, userpassword], (err,result)=>{
-        if(!err){
-            if(result.affectedRows > 0){
-                return res.status(200).json({message:"Successfully login,"});
-                
-            }else{
-                return res.status(200).json({message: result, codenumber: 0});
-            }
-            
-
-        }
-        return res.status(500).json({message:"server Error"});
-    });
 });
 
 app.get("/register", (req, res) => {
-    res.status(404).json({ message: "Not Found" });
+    res.sendFile(__dirname + "/client/register.html");
 });
 
 app.post("/register", (req, res) => {
-    const { username, userlast, useremail, userpassword, usercpasss } = req.body;
-    const sql = "INSERT INTO register(username, userlast, useremail, userpassword, usercpasss) VALUES (?,?,?,?,?)";
-    con.query(sql, [username, userlast, useremail, userpassword, usercpasss], (err, result) => {
+        const {first_name,last_name,email,password} = req.body;
+        console.log("Received POST request with data:", req.body);
+
+        const query = "INSERT INTO register(first_name,last_name,email,password) VALUES (?,?,?,?)";
+        con.query(query, [first_name,last_name,email,password], (err, result) => {
+            console.log(result);
+    
+            if(err){
+                console.error("Error executing database query:", err);
+                return res.status(500).json({message: "Server errror."});
+                
+            }else{
+                console.log("Database query result:", result);
+                return res.status(201).json({message: "registerd successfully"});
+                
+            }
+        });
+    });
+
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/client/login.html");
+});
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    const query = "SELECT user_id, first_name, last_name FROM register WHERE email = ? AND password = ?";
+
+    con.query(query, [email, password], (err, result) => {
         if (!err) {
-            return res.status(200).json({ message: "Successfully Created" });
+            if (result.length > 0) {
+                return res.status(200).json({ message: result, codeNumber: 1 });
+            }
+
+            return res.status(200).json({ message: "Invalid username or password", codeNumber: 0 });
         }
+
         return res.status(500).json({ message: "Server Error" });
     });
 });
-// app.get("/register",(req, res)=>{
-//     res.sendFile(__dirname + "/client/index.js");
-// })
-
-// app.post("/register",(req, res)=>{
-//     const {username, userlast , useremail, userpassword, usercpasss}=req.body;
-//     const sql = "INSERT INTO register(username, userlast , useremail, userpassword, usercpasss) VALUES (?,?,?,?,?)";
-//     con.query(sql, [username, userlast , useremail, userpassword, usercpasss], (err,result)=>{
-//         if(!err){
-//             return res.status(200).json({message:"Successfully Created,"});
-
-//         }
-//         return res.status(500).json({message:"server Error"});
-//     })
-// });
 
 
+
+
+app.get("/chat", (req, res) =>{
+    res.sendFile(__dirname + "/client/chat/chat.html");
+});
+
+
+app.get("/convo", (req, res) =>{
+    const sql = "SELECT user_id, message_text FROM message";
+
+    con.query(sql, (err, result) =>{
+        if(!err){
+            return res.status(200).json(result);
+        }
+
+        return res.status(500).json({message: "Server Error"});
+
+    });
+});
 
 io.on("connection", (socket) =>{
 
     console.log("connected");
 
-    socket.on("chat message", (messageObj) =>{
+    socket.on("chat", (messageObj) =>{
 
-        const {userid, message} =messageObj;
-        const sql ="INSERT INTO message(userid, ,message_text) VALUES (?,?)";
+        const { user_id, message_text } = messageObj;
 
-        con.query (sql, [userid, message], (err, result)=>{
+        console.log(messageObj);
+
+        const sql = "INSERT INTO message(user_id, message_text) VALUES(?, ?)";
+
+        con.query(sql, [user_id, message_text], (err, result)=>{
+            
             if(!err){
-                return io.emit("chat", messageObj);
-
+                io.emit("chat", messageObj);
             }
-            return io.emit("chat", err);
-        })
+            else{
+                io.emit("chat", {user_id: 0, message_text: "Server Error"});
+            }
+            
+        });
 
-        io.emit("chat message", messageObj);
-
+    
     });
 
     socket.on("disconnect", ()=>{
@@ -123,12 +128,7 @@ io.on("connection", (socket) =>{
 });
 
 
-
-
 server.listen(4000, () =>{
     console.log("Server listening on PORT 4000");
 });
-
-
-
-
+    
